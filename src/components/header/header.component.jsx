c1d986f5fa190139c75fa772551f63f507abf1af
renderVis <rendervis333@gmail.com>
2020-01-10 11:45:57 +0200

    implemented userReducer and userActions to header and App component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import "./header.styles.scss";
 
 import { auth } from "../../firebase/firebase.utils";
 
+import { connect } from "react-redux";
+
 const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
@@ -32,4 +34,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
